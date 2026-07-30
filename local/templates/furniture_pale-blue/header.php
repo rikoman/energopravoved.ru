20626805e3924773d440a5573bc1704115b3275d
Роман <rikoman702@gmail.com>
2026-07-30 21:30:46 +0700

    Шапка для мобилки

--- local/templates/furniture_pale-blue/header.php
+++ local/templates/furniture_pale-blue/header.php
@@ -29,7 +29,7 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/partners-carousel.js");
             <div class="bottom-block">
                 <div class="container">
                     <div class="bottom-grid">
-                        <!-- Логотип + название -->
+                        <!-- Логотип -->
                         <div class="bottom-col bottom-about">
                             <div class="bottom-logo">
                                 <div>
@@ -42,8 +42,17 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/partners-carousel.js");
                             </div>
                         </div>
 
-                        <!-- Меню -->
-                        <div class="bottom-col bottom-nav">
+                        <!-- Бургер (только мобилка) -->
+                        <div class="bottom-col bottom-burger">
+                            <button class="burger-btn" aria-label="Открыть меню">
+                                <span class="burger-line"></span>
+                                <span class="burger-line"></span>
+                                <span class="burger-line"></span>
+                            </button>
+                        </div>
+
+                        <!-- Обёртка: меню + контакты + соцсети + адрес -->
+                        <div class="bottom-col bottom-nav-wrapper">
                             <ul class="bottom-menu">
                                 <li><a href="#">Главная</a></li>
                                 <li><a href="#">О компании</a></li>
@@ -52,34 +61,31 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/partners-carousel.js");
                                 <li><a href="#">Контакты</a></li>
                                 <li><a href="#">Отзывы</a></li>
                             </ul>
-                        </div>
 
-                        <!-- Контакты (телефон, email) -->
-                        <div class="bottom-col bottom-contacts">
-                            <div class="bottom-contacts-left">
-                                <div class="bottom-contact-item">
-                                    <a href="tel:+79618956935">+7 (961) 895-69-35</a>
-                                </div>
-                                <div class="bottom-contact-item">
-                                    <a href="mailto:info@energopravoved.ru">info@energopravoved.ru</a>
+                            <div class="bottom-contacts">
+                                <div class="bottom-contacts-left">
+                                    <div class="bottom-contact-item">
+                                        <a href="tel:+79618956935">+7 (961) 895-69-35</a>
+                                    </div>
+                                    <div class="bottom-contact-item">
+                                        <a href="mailto:info@energopravoved.ru">info@energopravoved.ru</a>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
 
-                        <!-- Социальные сети (иконки) -->
-                        <div class="bottom-col bottom-social-block">
-                            <div class="bottom-social">
-                                <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/phone.svg" alt=""></a>
-                                <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/vk.svg" alt=""></a>
-                                <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/tg.svg" alt=""></a>
-                                <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/max.svg" alt=""></a>
+                            <div class="bottom-social-block">
+                                <div class="bottom-social">
+                                    <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/phone.svg" alt=""></a>
+                                    <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/vk.svg" alt=""></a>
+                                    <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/tg.svg" alt=""></a>
+                                    <a href="#" class="bottom-social-link"><img src="<?= SITE_TEMPLATE_PATH ?>/images/icons/max.svg" alt=""></a>
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- Адрес -->
-                        <div class="bottom-col bottom-address">
-                            <div class="bottom-contact-item">
-                                <span>Республика Хакасия<br>Красноярский край</span>
+                            <div class="bottom-address">
+                                <div class="bottom-contact-item">
+                                    <span>Республика Хакасия<br>Красноярский край</span>
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -87,3 +93,25 @@ Asset::getInstance()->addJs(SITE_TEMPLATE_PATH . "/js/partners-carousel.js");
             </div>
         </div>
     </header>
+    <script>
+        document.addEventListener('DOMContentLoaded', function() {
+            const burger = document.querySelector('.burger-btn');
+            const navWrapper = document.querySelector('.bottom-nav-wrapper');
+
+            if (burger && navWrapper) {
+                burger.addEventListener('click', function(e) {
+                    e.stopPropagation();
+                    this.classList.toggle('active');
+                    navWrapper.classList.toggle('open');
+                });
+
+                // Закрываем меню при клике вне его (опционально)
+                document.addEventListener('click', function(e) {
+                    if (!burger.contains(e.target) && !navWrapper.contains(e.target)) {
+                        burger.classList.remove('active');
+                        navWrapper.classList.remove('open');
+                    }
+                });
+            }
+        });
+    </script>
